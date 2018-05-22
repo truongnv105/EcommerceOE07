1d3221b89b9bafb323d18056eab8986b04d751be
nqngoc198 <nguyenquangngoc1981995@gmail.com>
2018-05-22 09:05:16 +0700

    List product and interface

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -44,37 +44,8 @@
     fade: true
   });
 
-  // PRODUCTS SLICK
-  $('#product-slick-1').slick({
-    slidesToShow: 3,
-    slidesToScroll: 2,
-    autoplay: true,
-    infinite: true,
-    speed: 300,
-    dots: true,
-    arrows: false,
-    appendDots: '.product-slick-dots-1',
-    responsive: [{
-        breakpoint: 991,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          dots: false,
-          arrows: true,
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        }
-      },
-    ]
-  });
-
   $('#product-slick-2').slick({
-    slidesToShow: 3,
+    slidesToShow: 4,
     slidesToScroll: 2,
     autoplay: true,
     infinite: true,
--- app/models/product.rb
+++ app/models/product.rb
@@ -5,4 +5,8 @@ class Product < ApplicationRecord
   has_many :ratings
   has_many :order_details
   has_many :orders, through: :order_details
+  scope :feature_hot, -> {where feature: true }
+  scope :order_create_at, -> {order(created_at: :DESC)}
+  scope :limit_num, -> (num){limit num}
+  scope :order_price, -> {order :price}
 end
